63b07a5876821f27833f97aff0210513540b62e9
dasda98 <wojniakjakub@gmail.com>
2024-06-11 11:52:28 +0200

    Modify main for particle system

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <time.h>
 
 #include <glad/glad.h>
 #include <cglm/cglm.h>
@@ -7,6 +8,7 @@
 
 
 #include <shader/shader.h>
+#include <particle/particle_system.h>
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -18,6 +20,8 @@ const unsigned int HEIGHT = 600;
 
 
 int main(void) {
+
+    srand(time(NULL));
 
     // GLFW
     glfwInit();
@@ -43,57 +47,33 @@ int main(void) {
 
     Shader myShader = createShader("../shaders/shader.vert", "../shaders/shader.frag");
 
-    float vertices[] = {
-        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-       -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top
-    };
-
-    unsigned int VBO, VAO;
-
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
+    ParticleSystem ps;
+    initParticleSystem(&ps);
 
     // Render loop
     while (!glfwWindowShouldClose(window)) {
+        // DeltaTime
+        float currentFrame = glfwGetTime();
+        static float lastFrame = 0.0f;
+        float deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         // Input
         processInput(window);
+        updateParticles(&ps, deltaTime);
 
         // Render
-        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Transform
-        mat4 transform = GLM_MAT4_IDENTITY_INIT;
-        glm_rotate(transform, (float)glfwGetTime(), (vec3){0.0f, 0.0f, 1.0f});
-
-        unsigned int transformLoc = glGetUniformLocation(myShader.ID, "transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, (const float*)transform);
-
-        // Render the triangle
         useShader(&myShader);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        renderParticles(&ps);
 
         // GLFW events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    deleteParticleSystem(&ps);
 
     glfwTerminate();
     return 0;
